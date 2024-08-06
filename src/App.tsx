2cb5d6871bb5b363e2b7e4e3a84af4f9888764ef
Danish Peer <peerdanish71@gmail.com>
2024-08-06 22:43:20 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white flex justify-center items-center h-screen">
+        Hi
+      </div>
+    </>
+  )
+}
+
+export default App
